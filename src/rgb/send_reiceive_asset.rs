ce41674c1710eb652d4d4ae45934568a204fa6e4
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-26 12:17:51 -0300

    Update and rename send_asset.rs to send_reiceive_asset.rs

---
+++ src/rgb/send_reiceive_asset.rs
@@ -0,0 +1,87 @@
+use bitcoin::{OutPoint, TxOut};
+use rgb::{
+    contract::{Contract, Extension, Genesis, Transition},
+    fungible::{AssetId, AssetType, Fungible},
+    nft::{Nft, NftId},
+    schema::{Bits, Data, FieldType, Schema},
+    util::amount::{Amount, Denomination},
+};
+use rgb_wallet::{
+    account::{AccountId, AccountInfo},
+    manager::{Manager, WalletEntry},
+    storage::{FileStorage, Storage},
+    wallet::{AddressPointer, Wallet},
+};
+use secp256k1::Secp256k1;
+use std::collections::HashMap;
+
+// Define the network you want to use, e.g. testnet or mainnet
+let network = bitcoin::network::constants::Network::Testnet;
+
+// Create a new wallet instance with an empty keychain and an in-memory signer
+let storage = FileStorage::new("wallet.dat".into()).unwrap();
+let manager = Manager::new(storage, network);
+let wallet_entry = WalletEntry::new("My Wallet".into());
+let wallet = Wallet::new(manager, wallet_entry).unwrap();
+
+// Define the RGB asset schema
+let schema = Schema {
+    field_types: vec![
+        FieldType::new("name".into(), Bits::Bit16, Data::String),
+        FieldType::new("description".into(), Bits::Bit16, Data::String),
+    ],
+    rgb_features: rgb::schema::features::RGBFeatures::Issuing,
+    doc: None,
+};
+
+// Define the RGB asset issuer
+let issuer = wallet.get_account("Issuer").unwrap().id;
+
+// Create a new RGB asset
+let asset = Fungible::new(
+    AssetType::Custom,
+    AssetId::new([0x01; 32]),
+    schema.clone(),
+    100000,
+    "My Token".into(),
+    "This is my custom token".into(),
+);
+
+// Issue 1000 tokens to the wallet's own address
+let initial_supply = Amount::from_denomination(1000, Denomination::Satoshis);
+let issue_txid = wallet
+    .issue_asset(issuer, &asset, initial_supply, "Initial issue".into())
+    .unwrap();
+
+// Wait for the transaction to confirm
+// Note: This is not strictly necessary if you only want to receive tokens and NFTs and not spend them immediately
+
+// Check the wallet's balance and transactions
+let (balance, txs) = wallet.sync().unwrap();
+
+// Print the wallet's balance to the console
+println!("Wallet balance: {}", balance);
+
+// Create a new NFT schema
+let nft_schema = Schema {
+    field_types: vec![
+        FieldType::new("name".into(), Bits::Bit16, Data::String),
+        FieldType::new("description".into(), Bits::Bit16, Data::String),
+    ],
+    rgb_features: rgb::schema::features::RGBFeatures::Nft,
+    doc: None,
+};
+
+// Define the RGB NFT issuer
+let nft_issuer = wallet.get_account("NFT Issuer").unwrap().id;
+
+// Create a new NFT
+let nft_id = NftId::new([0x02; 32]);
+let nft = Nft::new(nft_id, nft_schema.clone(), "My NFT".into(), "This is my custom NFT".into());
+
+// Issue the NFT to the wallet's own address
+let issue_txid = wallet.issue_nft(nft_issuer, &nft, "Initial issue".into()).unwrap();
+
+// Wait for the transaction to confirm
+// Note
+
